eaf9c768d079fcb39a1958ea59581f9baabaef7e
zsh git_setup.sh <leonie.elody@gmail.com>
2020-08-18 13:08:32 +0200

    Booking show and destroy

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -3,6 +3,10 @@ class BookingsController < ApplicationController
     @reservation = Reservation.new
   end
 
+  def show
+    @reservation = Reservation.find(params[:id])
+  end
+
   def create
     @bike = Bike.find(bike_params)
     @reservation = Reservation.new(reservation_params)
@@ -14,9 +18,19 @@ class BookingsController < ApplicationController
     end
   end
 
+  def destroy
+    @booking = Booking.find(params[:id])
+    @booking.destroy
+    redirect_to bike_path(@bike)
+  end
+
   private
 
   def bike_params
     params.require(:bike).permit(:model, :user_id, :address, :gender, :color, :gear, :description, :style, :price)
+  end
+
+  def find_booking
+    @booking = booking.find(params[:id])
   end
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,11 @@
 class Booking < ApplicationRecord
   belongs_to :bike
   belongs_to :user
+
+  validates :bike_id, presence: true
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validates :total_price, presence: true
+  validates :created_at, presence: true
+  validates :updated_at, presence: true
 end
