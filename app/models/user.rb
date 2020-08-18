18b8849412b044836d6b3a7233e9a6916544cfdc
Mathilde Nauche <m.nauche@gmail.com>
2020-08-18 13:00:26 +0200

    Check alert message and now you can sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :first_name, :last_name, :username, presence: true
-  validates :username, uniqueness: true
+  # validates :username, uniqueness: true
 end
