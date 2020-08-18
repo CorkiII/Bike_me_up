41d732db16679b1e4a1bfa284a58ec067ec7c27e
Mathilde Nauche <m.nauche@gmail.com>
2020-08-18 11:09:08 +0200

    Bookings controller new and create

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,22 @@
 class BookingsController < ApplicationController
+  def new
+    @reservation = Reservation.new
+  end
+
+  def create
+    @bike = Bike.find(bike_params)
+    @reservation = Reservation.new(reservation_params)
+    @reservation.bike = @reservation
+    if @bike.save
+      redirect_to bike_path(@bike)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def bike_params
+    params.require(:bike).permit(:model, :user_id, :address, :gender, :color, :gear, :description, :style, :price)
+  end
 end
