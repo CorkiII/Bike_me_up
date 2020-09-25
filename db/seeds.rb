4caaa6103cae9dc1ce484ef94b9d28a1bd6a5c7c
christophe mohammed-cherif <krisfromtama@gmail.com>
2020-09-25 14:20:35 +0200

    seed modifiee

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,11 +18,11 @@ User.create!(
   username: "chloer"
 )
 
-address = ['Nantes', 'Rennes', 'Lorient', 'Vannes', 'Arradon','Arzon', 'Sarzeau', 'Baden', 'Bono', 'Carnac', 'La Trinité sur mer']
+address = ['Nantes', 'Rennes', 'Paris', 'Vannes', 'Lyon','Marseille', 'Bordeaux', 'Nice', 'Strasbourg', 'Lille', 'Clermont-Ferrand']
 gender = ['homme', 'femme']
 style = ['VTC', 'VTT', 'Vélo de ville']
-description = ['Vélo de ville. Idéal pour les déplacements en ville pour la balade, les courses ou le travail.', 'Vélo à assistance électrique (VAE) pour tous vos déplacements afin d’arriver à destination sans fatigue et sans transpirer.', 'Vélo de randonnée. Idéal pour les balades et les randonnées sur tous types de revêtements et dans toutes les conditions.', 'Vélo de course. Idéal pour les balades sportives et les courses cyclistes.']
-model = ['Vélo B-Twin', 'Vélo piste Motobécane', 'Canyon spectral', 'Fixie Lejeune', 'Vélo de ville femme', 'VTT homme Btwin', 'VTT femme Go Sport', 'Vélo de ville femme Gitane', 'Vélo hollandais Peugeot', 'Randonneuse Helium', 'Vélo de ville Raymond Poulidor', 'Vélo Hangler']
+description = ['Idéal pour les déplacements en ville pour la balade, les courses ou le travail.', 'Idéal pour les balades et les randonnées sur tous types de revêtements et dans toutes les conditions.', 'Idéal pour les balades sportives et les courses cyclistes.']
+model = ['Vélo route', 'Vélo piste', 'Fixie', 'Vélo de ville', 'VTT', 'Vélo hollandais', 'Randonneuse']
 review = [ "Parfait ce vélo !", "En bon état, nous a suivi pendant tout notre tour du golf ! Je recommande", "Loueur très sympathique et vélo en parfait état. Pour des vacances avec style."]
 
 require 'open-uri'
@@ -39,7 +39,7 @@ html_doc.search('img').each do |element|
   image_urls << element.attribute('data-image').value if element.attribute('data-image')
 end
 
-image_urls.sample(100).each do |url|
+image_urls.sample(200).each do |url|
   file = URI.open(url)
   bike = Bike.create!(
     model: model.sample,
@@ -48,13 +48,13 @@ image_urls.sample(100).each do |url|
     color: Faker::Color.color_name,
     gear: rand(0..18),
     description: description.sample,
-    price: rand(10..50),
+    price: rand(15..30),
     style: style.sample,
     user: User.last)
   bike.photos.attach(io: file, filename: 'bike.jpg', content_type: 'image/jpg')
 end
 
-100.times do
+500.times do
   Review.create(bike: Bike.all.sample, content: review.sample, user: User.last)
 end
 
